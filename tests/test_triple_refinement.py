fda9d66715c63ba59870906921c60f415b83fb3d
Samarth Kulshreshtha <smkuls@gmail.com>
2018-08-31 18:23:57 -0500

    Implement triple refinement
    Issue: #15
    
    * This is simply a port from the Viff implementation.
    * There are still some methods which need to ironed out / optimized,
    there are TODOs left in appropriate places.

---
+++ tests/test_triple_refinement.py
@@ -0,0 +1,30 @@
+from pytest import mark
+import asyncio
+
+
+@mark.asyncio
+async def test_triple_refinement(triples_files_prefix):
+    from honeybadgermpc.passive import runProgramInNetwork, generate_test_triples
+    from honeybadgermpc.triple_refinement import refineTriples
+    N, t = 7, 3
+    # FIXME: This needs to be replaced with a fixture
+    generate_test_triples(triples_files_prefix, N, N, t)
+
+    async def _prog(context):
+        filename = f'{triples_files_prefix}-{context.myid}.share'
+        triple_shares = context.read_shares(open(filename))
+        _a, _b, _c = [], [], []
+        # Every party needs its share of all the `N` triples' shares
+        for i in range(0, 3 * context.N, 3):
+            p, q, pq = triple_shares[i:i+3]
+            _a.append(p.v), _b.append(q.v), _c.append(pq.v)
+        a, b, ab = await refineTriples(context, _a, _b, _c)
+        p = await asyncio.gather(*map(lambda x: x.open(), a))
+        q = await asyncio.gather(*map(lambda x: x.open(), b))
+        pq = await asyncio.gather(*map(lambda x: x.open(), ab))
+        assert len(p) == len(q) == len(pq), "Invalid number of values generated"
+        for d, e, de in zip(p, q, pq):
+            # print("\n[%d] %d * %d == %d" % (context.myid, d, e, de))
+            assert d * e == de
+
+    await runProgramInNetwork(_prog, N, t)
