60ba466cbd4ef451d4838060a5aa90b5babf68fa
Sylvain Bellemare <sbellem@gmail.com>
2018-10-11 01:20:57 -0500

    Parametrize ipc so that it can run in containers

--- scripts/launch_ipc_containers.sh
+++ scripts/launch_ipc_containers.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-# Assume hosts are as follows:
-"""
- 127.0.0.1   hbmpc_0
- 127.0.0.1   hbmpc_1
- 127.0.0.1   hbmpc_2
- 127.0.0.1   hbmpc_3
-"""
 
-CMD="python -m honeybadgermpc.ipc 4 1"
 set -x
+
+CMD="docker-compose -f network.yml up"
+
+docker-compose -f network.yml rm -sf
+
 tmux new-session     "${CMD} hbmpc_0; sh" \; \
      splitw -h -p 50 "${CMD} hbmpc_1; sh" \; \
      splitw -v -p 50 "${CMD} hbmpc_2; sh" \; \
