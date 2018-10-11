b177b8066e28363c00da854ecf5f263badae8dd5
Sylvain Bellemare <sbellem@gmail.com>
2018-10-11 17:08:58 -0500

    Run flake8 on all code

--- .ci/travis-script.sh
+++ .ci/travis-script.sh
@@ -5,7 +5,7 @@ BASE_CMD="docker-compose -f .travis.compose.yml run --rm test-hbmpc"
 if [ "${BUILD}" == "tests" ]; then
     $BASE_CMD pytest -v --cov=honeybadgermpc --cov-report=term-missing --cov-report=xml
 elif [ "${BUILD}" == "flake8" ]; then
-    flake8 honeybadgermpc/
+    flake8
 elif [ "${BUILD}" == "docs" ]; then
     sphinx-build -W -c docs -b html -d docs/_build/doctrees docs docs/_build/html
 fi
