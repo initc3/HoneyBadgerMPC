12a15d2b2b80a42d9288cf1fa188e5120a2ce0e1
Andrew Miller <socrates1024@gmail.com>
2018-10-29 15:22:28 -0500

    To make flake8 pass

--- scripts/run_hbavss.py
+++ scripts/run_hbavss.py
@@ -1,14 +1,15 @@
-from honeybadgermpc.betterpairing import G1
-from honeybadgermpc.secretshare_hbavsslight import *
+from honeybadgermpc.betterpairing import G1, ZR
+from honeybadgermpc.secretshare_hbavsslight import HbAvssDealer, HbAvssRecipient
 from honeybadgermpc.router import simple_router
 import asyncio
+
 
 def main():
     # TODO: We need to generate the CRS once and hardcode it as a parameter
     crs = [G1.rand(), G1.rand()]
     t = 2
     n = 3*t + 1
-    participantids = list(range(1,n+1))
+    participantids = list(range(1, n+1))
     dealerid = 0
     sid = 1
     (participantpubkeys, participantprivkeys) = ({}, {})
@@ -16,23 +17,24 @@ def main():
         sk = ZR.rand()
         participantprivkeys[i] = sk
         participantpubkeys[i] = crs[0] ** sk
-    pubparams = (t, n, crs, participantids, participantpubkeys, sid)
-    
+    pubparams = (t, n, crs, participantids, participantpubkeys, dealerid, sid)
+
     async def _test():
         sends, recvs = simple_router(len(participantids) + 1)
-        dealer = HbAvssDealer(pubparams, (42, dealerid), sends[dealerid], recvs[dealerid])
-        threads = []
-        #threads.append(HbAvssDealer(pubparams, (42, dealerid), sends[dealerid], recvs[dealerid]))
+        dealer = HbAvssDealer(pubparams, (42, dealerid),
+                              sends[dealerid], recvs[dealerid])
+        threads = [dealer.run()]
         recipients = []
         for i in participantids:
-            recipients.append(HbAvssRecipient(pubparams, (i, participantprivkeys[i]), sends[i], recvs[i]))
+            recipients.append(HbAvssRecipient(
+                pubparams, (i, participantprivkeys[i]), sends[i], recvs[i]))
         for r in recipients:
             threads.append(r.run())
         await asyncio.wait(threads)
-        
+
     loop = asyncio.get_event_loop()
     loop.run_until_complete(_test())
-    
+
 
 if __name__ == "__main__":
     debug = True
