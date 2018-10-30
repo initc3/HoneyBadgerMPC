21481de0ab4e32172dc15fdb10b73beeeab25005
Sylvain Bellemare <sbellem@gmail.com>
2018-10-30 22:05:11 +0000

    Use pytest asyncio marker for all tests
    Otherwise we somehow end up with two loops such that some tests run
    twice, one run being successful meanwhile the other fails.

--- tests/test_rand_protocol.py
+++ tests/test_rand_protocol.py
@@ -1,8 +1,13 @@
-def test_naive_():
-    from honeybadgermpc.rand_protocol import test_naive
-    test_naive()
+from pytest import mark
 
 
-def test_rand_():
-    from honeybadgermpc.rand_protocol import test_rand
-    test_rand()
+@mark.asyncio
+async def test_naive_():
+    from honeybadgermpc.rand_protocol import _test_naive
+    await _test_naive()
+
+
+@mark.asyncio
+async def test_rand_():
+    from honeybadgermpc.rand_protocol import _test_rand
+    await _test_rand()
