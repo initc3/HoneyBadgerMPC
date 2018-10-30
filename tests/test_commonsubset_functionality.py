c1a5df3faa88ab7569a1bf0de734b40db6de0c80
Sylvain Bellemare <sbellem@gmail.com>
2018-10-30 22:05:11 +0000

    Use pytest asyncio marker for all tests
    Otherwise we somehow end up with two loops such that some tests run
    twice, one run being successful meanwhile the other fails.

--- tests/test_commonsubset_functionality.py
+++ tests/test_commonsubset_functionality.py
@@ -1,3 +1,7 @@
-def test_acs_ideal_():
-    from honeybadgermpc.commonsubset_functionality import test_acs_ideal
-    test_acs_ideal()
+from pytest import mark
+
+
+@mark.asyncio
+async def test_acs_ideal_():
+    from honeybadgermpc.commonsubset_functionality import _test_acs_ideal
+    await _test_acs_ideal()
