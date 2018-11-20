df893141a806655166f3b8f55c0b13ccd884330d
lu562 <lu562@purdue.edu>
2018-11-19 17:51:04 -0800

    use thread pool on both uploading and downloading.

--- scripts/aws/download_input.sh
+++ scripts/aws/download_input.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 pids=""
-RESULT=0
+THREAD_NUM=200
+mkfifo tmp
+exec 9<>tmp
+for ((i = 0; i < $THREAD_NUM; i++))
+do
+    echo -ne "\n" 1>&9
+done
+
 
 # launch multiple curl in parallel to download inputs
 while IFS='' read -r line ; do
-    curl -sSO $line &
-    pids="$pids $!"
+    read -u 9
+    {
+	    curl -sSO $line 
+	    pids="$pids $!"
+	    echo -ne "\n" 1>&9
+	}&
+
 done < "$1"
 
 # wait for multiple curl to finish
 for pid in $pids; do
     wait $pid 
 done
+
+rm tmp
 
