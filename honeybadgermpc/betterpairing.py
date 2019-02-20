initc3/HoneyBadgerMPC

commit b65e7a414238c2bedfb93d302c26d5546b561a51

More feature to betterpairing added and some refactoring performed
1. Added some new methods in classes from betterpairing: G1, G2.
2. Added a new class to betterpairing: GT.
3. On the rust side, new class PyFq6 is added to lib.rs
4. New rust method vec_sum is added.
5. Some refactoring is performed on betterpairing.py

honeybadgermpc/betterpairing.py
@@ -1,5 +1,6 @@
-﻿from pypairing import PyFq, PyFq2, PyFqRepr, PyG1, PyG2, PyFr
+﻿from pypairing import PyFq, PyFq2, PyFq12, PyFqRepr, PyG1, PyG2, PyFr
 import random
+import re
 
 # Order of BLS group
 bls12_381_r = 52435875175126190479447740508185965837690552500527637822603658699938581184513  # (# noqa: E501)
@@ -23,6 +24,12 @@ def dupe_pyfr(pyfr):
     return out
 
 
+def dupe_pyfq12(pyfq12):
+    out = PyFq12("1")
+    out.copy(pyfq12)
+    return out
+
+
 class G1:
     def __init__(self, other=None):
         if other is None:
@@ -30,12 +37,12 @@ class G1:
         if type(other) is list:
             assert len(other) == 2
             assert len(other[0]) == 6
-            x = PyFqRepr(other[0][0], other[0][1], other[0]
-                         [2], other[0][3], other[0][4], other[0][5])
-            y = PyFqRepr(other[1][0], other[1][1], other[1]
-                         [2], other[1][3], other[1][4], other[1][5])
-            xq = PyFq(0, 0, 0, 1)
-            yq = PyFq(0, 0, 0, 1)
+            x = PyFqRepr(other[0][0], other[0][1], other[0][2],
+                         other[0][3], other[0][4], other[0][5])
+            y = PyFqRepr(other[1][0], other[1][1], other[1][2],
+                         other[1][3], other[1][4], other[1][5])
+            xq = PyFq()
+            yq = PyFq()
             xq.from_repr(x)
             yq.from_repr(y)
             self.pyg1 = PyG1()
@@ -48,13 +55,18 @@ class G1:
         y = int(self.pyg1.__str__()[108:206], 0)
         return "(" + str(x) + ", " + str(y) + ")"
 
+    def __repr__(self):
+        return str(self)
+
     def __mul__(self, other):
         if type(other) is G1:
             out = dupe_pyg1(self.pyg1)
             out.add_assign(other.pyg1)
             return G1(out)
         else:
-            raise TypeError
+            raise TypeError(
+                'Invalid multiplication param. Expected G1. Got '
+                + str(type(other)))
 
     def __imul__(self, other):
         if type(other) is G1:
@@ -67,7 +79,9 @@ class G1:
             out.sub_assign(other.pyg1)
             return G1(out)
         else:
-            raise TypeError
+            raise TypeError(
+                'Invalid division param. Expected G1. Got '
+                + str(type(other)))
 
     def __idiv__(self, other):
         if type(other) is G1:
@@ -81,11 +95,9 @@ class G1:
                 out.pyg1.zero()
                 return out
             if other < 0:
-                # out.invert()
                 out.pyg1.negate()
-                other = -1 * other
-            prodend = ZR(other)
-            out.pyg1.mul_assign(prodend.val)
+                other *= -1
+            out.pyg1.mul_assign(ZR(other).val)
             return out
         elif type(other) is ZR:
             out = G1(dupe_pyg1(self.pyg1))
@@ -107,12 +119,21 @@ class G1:
             self.pyg1.mul_assign(ZR(other).val)
             return self
         elif type(other) is ZR:
-            self.pyg1.mul_assign(other)
+            self.pyg1.mul_assign(other.val)
             return self
         else:
             raise TypeError(
                 'Invalid exponentiation param. Expected ZR or int. Got '
                 + str(type(other)))
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __rtruediv__(self, other):
+        return self.__truediv__(other)
+
+    def __rpow__(self, other):
+        return self.__pow__(other)
 
     def __eq__(self, other):
         if type(other) is not G1:
@@ -144,22 +165,25 @@ class G1:
     def projective(self):
         return self.pyg1.projective()
 
+    def pair_with(self, other):
+        fq12 = PyFq12()
+        self.pyg1.py_pairing_with(other.pyg2, fq12)
+        return GT(fq12)
+
     @staticmethod
     def one():
-        out = PyG1()
-        out.zero()
-        return G1(out)
+        return G1(PyG1().zero())
 
     @staticmethod
     def rand(seed=None):
         out = PyG1()
         if seed is None:
             seed = []
-            for i in range(4):
+            for _ in range(4):
                 seed.append(random.SystemRandom().randint(0, 4294967295))
             out.rand(seed[0], seed[1], seed[2], seed[3])
         else:
-            assert type(seed) is list
+            assert seed is list
             assert len(seed) == 4
             out.rand(seed[0], seed[1], seed[2], seed[3])
         return G1(out)
@@ -172,16 +196,16 @@ class G2:
         if type(other) is list:
             assert len(other) == 4
             assert len(other[0]) == 6
-            x1 = PyFqRepr(other[0][0], other[0][1], other[0]
-                          [2], other[0][3], other[0][4], other[0][5])
-            x2 = PyFqRepr(other[1][0], other[1][1], other[1]
-                          [2], other[1][3], other[1][4], other[1][5])
-            y1 = PyFqRepr(other[2][0], other[2][1], other[2]
-                          [2], other[2][3], other[2][4], other[2][5])
-            y2 = PyFqRepr(other[3][0], other[3][1], other[3]
-                          [2], other[3][3], other[3][4], other[3][5])
-            xq = PyFq2(0, 0, 0, 1)
-            yq = PyFq2(0, 0, 0, 1)
+            x1 = PyFqRepr(other[0][0], other[0][1], other[0][2],
+                          other[0][3], other[0][4], other[0][5])
+            x2 = PyFqRepr(other[1][0], other[1][1], other[1][2],
+                          other[1][3], other[1][4], other[1][5])
+            y1 = PyFqRepr(other[2][0], other[2][1], other[2][2],
+                          other[2][3], other[2][4], other[2][5])
+            y2 = PyFqRepr(other[3][0], other[3][1], other[3][2],
+                          other[3][3], other[3][4], other[3][5])
+            xq = PyFq2()
+            yq = PyFq2()
             xq.from_repr(x1, x2)
             yq.from_repr(y1, y2)
             self.pyg2 = PyG2()
@@ -196,6 +220,9 @@ class G2:
         y1 = int(out[226:324], 0)
         y2 = int(out[331:429], 0)
         return "(" + str(x1) + " + " + str(x2) + "u, " + str(y1) + " + " + str(y2) + "u)"
+
+    def __repr__(self):
+        return str(self)
 
     def __mul__(self, other):
         if type(other) is G2:
@@ -214,7 +241,9 @@ class G2:
             out.sub_assign(other.pyg2)
             return G2(out)
         else:
-            raise TypeError
+            raise TypeError(
+                'Invalid division param. Expected G1. Got '
+                + str(type(other)))
 
     def __idiv__(self, other):
         if type(other) is G2:
@@ -228,11 +257,9 @@ class G2:
                 out.pyg2.zero()
                 return out
             if other < 0:
-                # out.invert()
                 out.pyg2.negate()
-                other = -1 * other
-            prodend = ZR(other)
-            out.pyg2.mul_assign(prodend.val)
+                other *= -1
+            out.pyg2.mul_assign(ZR(other).val)
             return out
         elif type(other) is ZR:
             out = G2(dupe_pyg2(self.pyg2))
@@ -254,12 +281,21 @@ class G2:
             self.pyg2.mul_assign(ZR(other).val)
             return self
         elif type(other) is ZR:
-            self.pyg2.mul_assign(other)
+            self.pyg2.mul_assign(other.val)
             return self
         else:
             raise TypeError(
                 'Invalid exponentiation param. Expected ZR or int. Got '
                 + str(type(other)))
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __rtruediv__(self, other):
+        return self.__truediv__(other)
+
+    def __rpow__(self, other):
+        return self.__pow__(other)
 
     def __eq__(self, other):
         if type(other) is not G2:
@@ -293,16 +329,14 @@ class G2:
 
     @staticmethod
     def one():
-        out = PyG2()
-        out.zero()
-        return G2(out)
+        return G2(PyG2().zero())
 
     @staticmethod
     def rand(seed=None):
         out = PyG2()
         if seed is None:
             seed = []
-            for i in range(4):
+            for _ in range(4):
                 seed.append(random.SystemRandom().randint(0, 4294967295))
             out.rand(seed[0], seed[1], seed[2], seed[3])
         else:
@@ -312,18 +346,128 @@ class G2:
         return G2(out)
 
 
+class GT:
+    def __init__(self, other=None):
+        if other is None:
+            self.pyfq12 = PyFq12()
+            self.pyfq12.rand(1, 0, 0, 0)
+        elif type(other) is PyFq12:
+            self.pyfq12 = other
+        elif type(other) is list:
+            assert len(other) == 12
+            self.pyfq12 = PyFq12()
+            self.pyfq12.from_strs(*other)
+        elif type(other) is int:
+            self.pyfq12 = PyFq12()
+            self.pyfq12.from_strs(str(other), *["0"]*11)
+        elif type(other) is str:
+            lst = [x.strip() for x in other.split(',')]
+            assert len(lst) == 12
+            if lst[0][1] == 'x':
+                for i in range(len(lst)):
+                    lst[i] = str(int(lst[i]))
+            self.pyfq12 = PyFq12()
+            self.pyfq12.from_strs(*lst)
+
+    def __mul__(self, other):
+        if type(other) is int:
+            out = GT(dupe_pyfq12(self.pyfq12))
+            if other == 0:
+                out.pyfq12.zero()
+                return out
+            if other < 0:
+                out.pyfq12.negate()
+                other = -1 * other
+            prodend = GT(other)
+            out.pyfq12.mul_assign(prodend.pyfq12)
+            return out
+        elif type(other) is ZR:
+            out = GT(dupe_pyfq12(self.pyfq12))
+            out.pyfq12.mul_assign(other.val)
+            return out
+        elif type(other) is GT:
+            out = GT(dupe_pyfq12(self.pyfq12))
+            out.pyfq12.mul_assign(other.pyfq12)
+            return out
+        else:
+            raise TypeError(
+                'Invalid exponentiation param. Expected ZR or int. Got '
+                + str(type(other)))
+
+    def __add__(self, other):
+        if type(other) is GT:
+            out = dupe_pyfq12(self.pyfq12)
+            out.add_assign(other.pyfq12)
+            return G2(out)
+
+    # TODO: __truediv__ needs to be implemented
+
+    # TODO: __pow__ needs to be implemented
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __rtruediv__(self, other):
+        return self.__truediv__(other)
+
+    def __rpow__(self, other):
+        return self.__pow__(other)
+
+    def __str__(self):
+        out = self.pyfq12.__str__()
+        return out
+
+    def __eq__(self, other):
+        if type(other) is not GT:
+            return False
+        return self.pyfq12.equals(other.pyfq12)
+
+    def __getstate__(self):
+        s = self.pyfq12.__str__()
+        s = s.replace("Fq6", "")
+        s = s.replace("Fq2", "")
+        s = s.replace("Fq", "")
+        s = s.replace(" ", "")
+        s = s.replace("*", "")
+        s = s.replace("u", "")
+        s = s.replace("v^2", "")
+        s = s.replace("v", "")
+        s = s.replace("w", "")
+        s = s.replace("(", "")
+        s = s.replace(")", "")
+        s = s.replace("+", ",")
+        return re.sub("0x0*", "0x0", s)
+
+    def __setstate__(self, d):
+        self.__init__(d)
+
+    @staticmethod
+    def rand(seed=None):
+        out = PyFq12()
+        if seed is None:
+            seed = []
+            for _ in range(4):
+                seed.append(random.SystemRandom().randint(0, 4294967295))
+            out.rand(seed[0], seed[1], seed[2], seed[3])
+        else:
+            assert seed is list
+            assert len(seed) == 4
+            out.rand(seed[0], seed[1], seed[2], seed[3])
+        return GT(out)
+
+
 class ZR:
     def __init__(self, val=None):
         self.pp = []
         if val is None:
             self.val = PyFr(0x5dbe6259, 0x8d313d76, 0x3237db17, 0xe5bc0654)
         elif type(val) is int:
+            self.val = PyFr(str(abs(val)))
             if val < 0:
-                val = val * -1
-                self.val = PyFr(str(val))
                 self.val.negate()
-            else:
-                self.val = PyFr(str(val))
         elif type(val) is str:
             if val[1] == 'x':
                 self.val = PyFr(val)
@@ -339,6 +483,9 @@ class ZR:
     def __str__(self):
         hexstr = self.val.__str__()[3:-1]
         return str(int(hexstr, 0))
+
+    def __repr__(self):
+        return str(self)
 
     def __int__(self):
         hexstr = self.val.__str__()[3:-1]
@@ -365,18 +512,18 @@ class ZR:
                 + str(type(other)))
 
     def __iadd__(self, other):
+        self.pp = []
         if type(other) is ZR:
             self.val.add_assign(other.val)
             return self
         elif type(other) is int:
             if other < 0:
-                other *= 1
+                other *= -1
                 addend = PyFr(str(other))
                 addend.negate()
             else:
                 addend = PyFr(str(other))
             self.val.add_assign(addend)
-            self.pp = []
             return self
         else:
             raise TypeError(
@@ -404,18 +551,18 @@ class ZR:
                 + str(type(other)))
 
     def __isub__(self, other):
+        self.pp = []
         if type(other) is ZR:
             self.val.sub_assign(other.val)
             return self
         elif type(other) is int:
             if other < 0:
-                other *= 1
+                other *= -1
                 subend = PyFr(str(other))
                 subend.negate()
             else:
                 subend = PyFr(str(other))
             self.val.sub_assign(subend)
-            self.pp = []
             return self
         else:
             raise TypeError(
@@ -443,6 +590,7 @@ class ZR:
                 + str(type(other)))
 
     def __imul__(self, other):
+        self.pp = []
         if type(other) is ZR:
             self.val.mul_assign(other.val)
             return self
@@ -454,7 +602,6 @@ class ZR:
             else:
                 prodend = PyFr(str(other))
             self.val.mul_assign(prodend)
-            self.pp = []
             return self
         else:
             raise TypeError(
@@ -486,44 +633,32 @@ class ZR:
 
     def __pow__(self, other):
         if type(other) is int:
-            if other == 0:
-                return ZR(1)
             other = other % (bls12_381_r-1)
-            out = dupe_pyfr(self.val)
-            if self.pp == []:
-                self.init_pp()
-            i = 0
-            # Hacky solution to my off by one error
-            other -= 1
-            while other > 0:
-                if other % 2 == 1:
-                    out.mul_assign(self.pp[i])
-                i += 1
-                other = other >> 1
-            return ZR(out)
         elif type(other) is ZR:
-            out = dupe_pyfr(self.val)
-            if self.pp == []:
-                self.init_pp()
-            i = 0
-            # Hacky solution to my off by one error
             other = int(other)
-            other -= 1
-            while other > 0:
-                if other % 2 == 1:
-                    out.mul_assign(self.pp[i])
-                i += 1
-                other = other >> 1
-            return ZR(out)
         else:
             raise TypeError(
                 'Invalid multiplication param. Expected int or ZR. Got '
                 + str(type(other)))
+        if other == 0:
+            return ZR(1)
+        out = dupe_pyfr(self.val)
+        if self.pp == []:
+            self.init_pp()
+        i = 0
+        # Hacky solution to my off by one error
+        other -= 1
+        while other > 0:
+            if other % 2 == 1:
+                out.mul_assign(self.pp[i])
+            i += 1
+            other = other >> 1
+        return ZR(out)
 
     def __eq__(self, other):
         if type(other) is not ZR:
             return False
-        return self.pyg1.equals(other.val)
+        return self.val.equals(other.val)
 
     def __getstate__(self):
         return int(self)
@@ -551,10 +686,8 @@ class ZR:
 
     @staticmethod
     def zero():
-        out = PyFr("0")
-        return ZR(out)
+        return ZR(PyFr("0"))
 
     @staticmethod
     def one():
-        out = PyFr("1")
-        return ZR(out)
+        return ZR(PyFr("1"))

pairing/pypairing/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-from .pypairing import PyG1, PyFr, PyG2, py_pairing, PyFq, PyFq2, PyFq12, PyFqRepr
+from .pypairing import (PyG1, PyFr, PyG2, py_pairing, PyFq, PyFq2, PyFq12, PyFqRepr,
+                        vec_sum)
 
-__all__ = ["PyG1", "PyFr", "PyG2", "py_pairing", "PyFq", "PyFq2", "PyFq12", "PyFqRepr"]
+__all__ = ["PyG1", "PyFr", "PyG2", "py_pairing", "PyFq", "PyFq2", "PyFq12", "PyFqRepr",
+           "vec_sum"]

pairing/src/lib.rs
@@ -27,7 +27,7 @@ extern crate rand;
 pub mod tests;
 
 pub mod bls12_381;
-use bls12_381::{G1, G2, Fr, Fq, Fq2, Fq12, FqRepr};
+use bls12_381::{G1, G2, Fr, Fq, Fq2, Fq6, Fq12, FqRepr};
 mod wnaf;
 pub use self::wnaf::Wnaf;
 
@@ -397,6 +397,11 @@ impl PyFr {
         self.fr.square();
         Ok(())
     }
+    
+    fn pow(&mut self, s1: u64, s2: u64, s3: u64, s4: u64, s5: u64, s6:u64) -> PyResult<()> {
+        self.fr.pow([s1,s2,s3,s4,s5,s6]);
+        Ok(())
+    }
 
     fn add_assign(&mut self, other: &Self) -> PyResult<()> {
         self.fr.add_assign(&other.fr);
@@ -437,9 +442,11 @@ struct PyFq {
  #[pymethods]
 impl PyFq {
     #[new]
-    fn __new__(obj: &PyRawObject, s1: u32, s2: u32, s3: u32, s4: u32) -> PyResult<()>{
-        let mut rng = XorShiftRng::from_seed([s1,s2,s3,s4]);
-        let f =  Fq::rand(&mut rng);
+    //fn __new__(obj: &PyRawObject, s1: u32, s2: u32, s3: u32, s4: u32) -> PyResult<()>{
+    fn __new__(obj: &PyRawObject) -> PyResult<()>{
+        //let mut rng = XorShiftRng::from_seed([s1,s2,s3,s4]);
+        //let f =  Fq::rand(&mut rng);
+        let f =  Fq::zero();
         obj.init(|t| PyFq{
             fq: f,
         })
@@ -458,9 +465,11 @@ struct PyFq2 {
  #[pymethods]
 impl PyFq2 {
     #[new]
-    fn __new__(obj: &PyRawObject, s1: u32, s2: u32, s3: u32, s4: u32) -> PyResult<()>{
-        let mut rng = XorShiftRng::from_seed([s1,s2,s3,s4]);
-        let f =  Fq2::rand(&mut rng);
+    //fn __new__(obj: &PyRawObject, s1: u32, s2: u32, s3: u32, s4: u32) -> PyResult<()>{
+    fn __new__(obj: &PyRawObject) -> PyResult<()>{
+        //let mut rng = XorShiftRng::from_seed([s1,s2,s3,s4]);
+        //let f =  Fq2::rand(&mut rng);
+        let f =  Fq2::zero();
         obj.init(|t| PyFq2{
             fq2: f,
         })
@@ -471,6 +480,24 @@ impl PyFq2 {
         self.fq2.c0 = c0;
         self.fq2.c1 = c1;
         Ok(())
+    }
+}
+
+#[pyclass]
+struct PyFq6 {
+    fq6 : Fq6
+}
+ #[pymethods]
+impl PyFq6 {
+    #[new]
+    //fn __new__(obj: &PyRawObject, s1: u32, s2: u32, s3: u32, s4: u32) -> PyResult<()>{
+    fn __new__(obj: &PyRawObject) -> PyResult<()>{
+        //let mut rng = XorShiftRng::from_seed([s1,s2,s3,s4]);
+        //let f =  Fq2::rand(&mut rng);
+        let f =  Fq6::zero();
+        obj.init(|t| PyFq6{
+            fq6: f,
+        })
     }
 }
 
@@ -498,15 +525,52 @@ struct PyFq12 {
 impl PyFq12 {
     #[new]
     fn __new__(obj: &PyRawObject) -> PyResult<()>{
+        let q =  Fq12::zero();
+        obj.init(|t| PyFq12{
+            fq12: q,
+        })
+        //Ok(())
+    }
+    fn from_strs(&mut self, s1: &str, s2: &str, s3: &str, s4: &str, s5: &str, s6: &str, s7: &str, s8: &str, s9: &str, s10: &str, s11: &str, s12: &str) -> PyResult<()> {
+        let c0 = Fq6 {
+            c0: Fq2 {
+                c0: Fq::from_str(s1).unwrap(),
+                c1: Fq::from_str(s2).unwrap()
+            },
+            c1: Fq2 {
+                c0: Fq::from_str(s3).unwrap(),
+                c1: Fq::from_str(s4).unwrap()
+            },
+            c2: Fq2 {
+                c0: Fq::from_str(s5).unwrap(),
+                c1: Fq::from_str(s6).unwrap()
+            }
+        };
+        let c1 = Fq6 {
+            c0: Fq2 {
+                c0: Fq::from_str(s7).unwrap(),
+                c1: Fq::from_str(s8).unwrap()
+            },
+            c1: Fq2 {
+                c0: Fq::from_str(s9).unwrap(),
+                c1: Fq::from_str(s10).unwrap()
+            },
+            c2: Fq2 {
+                c0: Fq::from_str(s11).unwrap(),
+                c1: Fq::from_str(s12).unwrap()
+            }
+        };
+        self.fq12.c0 = c0;
+        self.fq12.c1 = c1;
         Ok(())
     }
-
+    
     pub fn __str__(&self) -> PyResult<String> {
-        Ok(format!("Fq12({} + {} * w)",self.fq12.c0, self.fq12.c1 ))
+        Ok(format!("({} + {} * w)",self.fq12.c0, self.fq12.c1 ))
     }
 
     pub fn __repr__(&self) -> PyResult<String> {
-        Ok(format!("Fq12({} + {} * w)",self.fq12.c0, self.fq12.c1 ))
+        Ok(format!("({} + {} * w)",self.fq12.c0, self.fq12.c1 ))
     }
 
     fn rand(&mut self, s1: u32, s2: u32, s3: u32, s4: u32) -> PyResult<()> {
@@ -530,6 +594,16 @@ impl PyFq12 {
         self.fq12.mul_assign(&other.fq12);
         Ok(())
     }
+    
+    fn one(&mut self) -> PyResult<()> {
+        self.fq12 = Fq12::one();
+        Ok(())
+    }
+   
+    fn zero(&mut self) -> PyResult<()> {
+        self.fq12 = Fq12::zero();
+        Ok(())
+    }
 
     fn equals(&self, other: &Self) -> bool {
         self.fq12 == other.fq12
@@ -540,10 +614,17 @@ impl PyFq12 {
         self.fq12 = other.fq12;
         Ok(())
     }
-
-
 }
 
+#[pyfunction]
+fn vec_sum(a: &PyList) -> PyResult<String>{
+    let mut sum =  Fr::from_str("0").unwrap();
+    for item in a.iter(){
+        let myfr: &PyFr = item.try_into().unwrap();
+        sum.add_assign(&myfr.fr);
+    }
+    Ok(format!("{}",sum))
+}
 
 #[pymodinit]
 fn pypairing(py: Python, m: &PyModule) -> PyResult<()> {
@@ -552,9 +633,12 @@ fn pypairing(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<PyFq>()?;
     m.add_class::<PyFqRepr>()?;
     m.add_class::<PyFq2>()?;
+    m.add_class::<PyFq6>()?;
     m.add_class::<PyFq12>()?;
     m.add_class::<PyFr>()?;
     m.add_function(wrap_function!(py_pairing)).unwrap();
+    //m.add_function(wrap_function!(vec_sum))?;
+    m.add_function(wrap_function!(vec_sum)).unwrap();
     Ok(())
 }
 

commit fafe4d6081308a6d5e89fb35e7fa188f74347a21

Fix an assert issue caused by #177

honeybadgermpc/betterpairing.py
@@ -183,7 +183,7 @@ class G1:
                 seed.append(random.SystemRandom().randint(0, 4294967295))
             out.rand(seed[0], seed[1], seed[2], seed[3])
         else:
-            assert seed is list
+            assert type(seed) is list
             assert len(seed) == 4
             out.rand(seed[0], seed[1], seed[2], seed[3])
         return G1(out)
@@ -340,7 +340,7 @@ class G2:
                 seed.append(random.SystemRandom().randint(0, 4294967295))
             out.rand(seed[0], seed[1], seed[2], seed[3])
         else:
-            assert seed is list
+            assert type(seed) is list
             assert len(seed) == 4
             out.rand(seed[0], seed[1], seed[2], seed[3])
         return G2(out)
@@ -453,7 +453,7 @@ class GT:
                 seed.append(random.SystemRandom().randint(0, 4294967295))
             out.rand(seed[0], seed[1], seed[2], seed[3])
         else:
-            assert seed is list
+            assert type(seed) is list
             assert len(seed) == 4
             out.rand(seed[0], seed[1], seed[2], seed[3])
         return GT(out)