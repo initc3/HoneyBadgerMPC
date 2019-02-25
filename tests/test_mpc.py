a50a9e3db6c38ce2f9486040a8ec2c38c08758eb
hanyunxu <hanyunx2@illinois.edu>
2019-02-25 13:19:15 -0600

    add xor test

--- tests/test_mpc.py
+++ tests/test_mpc.py
@@ -24,3 +24,24 @@ async def test_open_shares(test_preprocessing):
     assert len(results) == n
     assert all(len(secrets) == number_of_secrets for secrets in results)
     assert all(secret == 0 for secrets in results for secret in secrets)
+
+
+async def test_xor(test_preprocessing):
+    from honeybadgermpc.mpc import TaskProgramRunner
+
+    n, t = 3, 1
+    number_of_secrets = 100
+
+    async def _prog(context):
+        x = context.Share(10)
+        y = context.Share(5)
+        z = context.field(5)
+
+        xor1 = x^y
+        xor2 = x^z
+
+        assert xor1 == context.Share(15)
+        assert xor2 == context.field(15)
+
+
+    
