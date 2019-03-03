35a13a2d070a37409734b0bc27bc7a7516e6b6d5
Samarth Kulshreshtha <smkuls@gmail.com>
2019-03-03 17:54:45 -0600

    Add `benchmark` folder to aws/__init__.py
    This is needed to be able to load the logging config without any errors.

--- aws/__init__.py
+++ aws/__init__.py
@@ -1,8 +1,10 @@
 import logging.config
 import yaml
+import os
 
 
 with open('honeybadgermpc/logging.yaml', 'r') as f:
+    os.makedirs("benchmark", exist_ok=True)
     logging_config = yaml.safe_load(f.read())
     logging.config.dictConfig(logging_config)
     logging.getLogger('paramiko').setLevel(logging.WARNING)
