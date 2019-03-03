2475012b5855f090a985c84e3bcee1078e82969c
Samarth Kulshreshtha <smkuls@gmail.com>
2019-03-03 17:55:30 -0600

    Optimize evaluation of polynomials by using ntl
    Switch to vandermonde evaluate exposed via NTL for evaluating the
    polynomial at a range of values. This would bring down the amount of
    time required to create the preprocessing files.

--- honeybadgermpc/preprocessing.py
+++ honeybadgermpc/preprocessing.py
@@ -6,6 +6,7 @@ from random import randint
 from os import makedirs
 from .field import GF
 from .polynomial import polynomials_over
+from .ntl.helpers import batch_vandermonde_evaluate
 from .elliptic_curve import Subgroup
 
 
@@ -52,8 +53,11 @@ class PreProcessedElements(object):
         f.write(content)
 
     def _write_polys(self, file_name_prefix, n, t, polys):
+        polys = [[coeff.value for coeff in poly.coeffs] for poly in polys]
+        all_shares = batch_vandermonde_evaluate(
+            list(range(1, n+1)), polys, self.field.modulus)
         for i in range(n):
-            shares = [f(i+1) for f in polys]
+            shares = [self.field(s[i]) for s in all_shares]
             with open('%s_%d_%d-%d.share' % (file_name_prefix, n, t, i), 'w') as f:
                 self._write_shares_to_file(f, t, i, shares)
 
