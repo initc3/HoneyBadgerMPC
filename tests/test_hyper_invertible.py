73fb9220dd29bb2be63b76b43e81c9ab2bb672ac
Andrew Miller <socrates1024@gmail.com>
2019-05-13 09:37:55 -0500

    Uses batch encode/decode for hyp.inv. double sharing

--- tests/test_hyper_invertible.py
+++ tests/test_hyper_invertible.py
@@ -7,14 +7,15 @@ from honeybadgermpc.reed_solomon import Algorithm, DecoderFactory
 
 @mark.asyncio
 @mark.parametrize("n", [4, 7])
-async def test_generate_double_shares(test_router, polynomial, galois_field, n):
+@mark.parametrize("k", [1, 10])
+async def test_generate_double_shares(test_router, polynomial, galois_field, n, k):
     t = (n-1)//3
     sends, receives, _ = test_router(n)
     shares_per_party = await asyncio.gather(*[generate_double_shares(
-        n, t, i, sends[i], receives[i], galois_field) for i in range(n)])
+        n, t, i, sends[i], receives[i], galois_field, k) for i in range(n)])
     assert len(shares_per_party) == n
-    assert all(len(random_shares) == n-2*t for random_shares in shares_per_party)
-    random_values = [None]*(n-2*t)
+    assert all(len(random_shares) == (n-2*t)*k for random_shares in shares_per_party)
+    random_values = []
     eval_point = EvalPoint(galois_field, n, use_fft=True)
     decoder = DecoderFactory.get(eval_point, Algorithm.FFT)
     for i, shares in enumerate(zip(*shares_per_party)):
@@ -22,5 +23,5 @@ async def test_generate_double_shares(test_router, polynomial, galois_field, n):
         r_t = polynomial(decoder.decode(list(range(n)), shares_t))(0)
         r_2t = polynomial(decoder.decode(list(range(n)), shares_2t))(0)
         assert r_t == r_2t
-        random_values[i] = r_t
-    assert len(set(random_values)) == n-2*t
+        random_values.append(r_t)
+    assert len(set(random_values)) == (n-2*t) * k
