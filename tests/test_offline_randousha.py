52c37f2a1b78174940a90e475dcae4ff39e37b14
Andrew Miller <socrates1024@gmail.com>
2019-05-25 20:50:15 -0500

    test case showing double decoding doesn't work as expected

--- tests/test_offline_randousha.py
+++ tests/test_offline_randousha.py
@@ -25,3 +25,24 @@ async def test_randousha(test_router, polynomial, galois_field, n, k):
         assert r_t == r_2t
         random_values.append(r_t)
     assert len(set(random_values)) == (n-2*t) * k
+
+
+@mark.asyncio
+@mark.parametrize("n", [4, 7])
+@mark.parametrize("k", [1, 10])
+async def test_double_decode(n, k, galois_field, test_router, test_runner):
+    t = (n-1)//3
+    sends, receives, _ = test_router(n)
+    shares_per_party = await asyncio.gather(*[randousha(
+        n, t, k, i, sends[i], receives[i], galois_field) for i in range(n)])
+
+    async def _prog(context):
+        # Every party needs its share of all the `N` triples' shares
+        shares = shares_per_party[context.myid]
+        shares_t, shares_2t = zip(*shares)
+        rs_t = await context.ShareArray(shares_t).open()
+        rs_2t = await context.ShareArray(shares_2t, 2*t).open()
+        print('ok')
+
+    await test_runner(_prog, n, t)
+
