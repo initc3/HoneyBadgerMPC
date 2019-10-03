3be4c47a68a5240243fcfe4b03d7500add6acccf
sanket1729 <sanket1729@gmail.com>
2019-10-02 22:35:28 -0500

    ANother travis for building from non source PRs

--- scripts/docker_login.sh
+++ scripts/docker_login.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo $TRAVIS_REPO_SLUG;
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]  || [ "$TRAVIS_REPO_SLUG" == "initc3/HoneyBadgerMPC" ]
+echo $TRAVIS_PULL_REQUEST_SLUG;
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]  || [ "$TRAVIS_PULL_REQUEST_SLUG" == "initc3/HoneyBadgerMPC" ]
  then
  	echo "Logging in with docker credentials"; 
  	docker login -p $DOCKER_PASS -u $DOCKER_USER 
