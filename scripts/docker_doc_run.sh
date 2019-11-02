c3123d465cd3de2e3f20c0433bed1495e3bb1e3d
Sylvain Bellemare <sbellem@gmail.com>
2019-11-02 02:33:51 -0400

    Troubleshoot

--- scripts/docker_doc_run.sh
+++ scripts/docker_doc_run.sh
@@ -2,10 +2,15 @@
 
 set -ev
 
-SPHINX_BUILD_CMD=sphinx-build -M html docs docs/_build -c docs -W -v --keep-going
+SPHINX_BUILD_CMD="sphinx-build -M html docs docs/_build -c docs -W -v --keep-going"
+IMAGE_TAG=latest
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]  || [ "$TRAVIS_PULL_REQUEST_SLUG" == "initc3/HoneyBadgerMPC" ]; then
- docker run -it dsluiuc/honeybadger-prod:$TRAVIS_COMMIT $SPHINX_BUILD_CMD
-else 
- docker run -it dsluiuc/honeybadger-prod:latest $SPHINX_BUILD_CMD
+    IMAGE_TAG=$TRAVIS_COMMIT
 fi
+# docker run -it dsluiuc/honeybadger-prod:$TRAVIS_COMMIT $SPHINX_BUILD_CMD
+#else 
+# docker run -it dsluiuc/honeybadger-prod:latest $SPHINX_BUILD_CMD
+#fi
+ 
+docker run -it dsluiuc/honeybadger-prod:$IMAGE_TAG $SPHINX_BUILD_CMD
