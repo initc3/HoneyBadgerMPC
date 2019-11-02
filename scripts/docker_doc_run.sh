af26ab934ad2a6e9d6ba9826357c6b7a976859d3
Sylvain Bellemare <sbellem@gmail.com>
2019-11-02 01:50:10 -0400

    Add verbosity to sphinx-build cmd

--- scripts/docker_doc_run.sh
+++ scripts/docker_doc_run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+SPHINX_BUILD_CMD=sphinx-build -M html docs docs/_build -c docs -W -v --keep-going
+
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]  || [ "$TRAVIS_PULL_REQUEST_SLUG" == "initc3/HoneyBadgerMPC" ]; then
- docker run -it dsluiuc/honeybadger-prod:$TRAVIS_COMMIT sphinx-build -M html docs docs/_build -c docs -W
+ docker run -it dsluiuc/honeybadger-prod:$TRAVIS_COMMIT $SPHINX_BUILD_CMD
 else 
- docker run -it dsluiuc/honeybadger-prod:latest sphinx-build -M html docs docs/_build -c docs -W
+ docker run -it dsluiuc/honeybadger-prod:latest $SPHINX_BUILD_CMD
 fi
