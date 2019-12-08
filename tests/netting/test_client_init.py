732de0947ef37723dc3c8dc2f76bca9c24d0c09a
sanket1729 <sanket1729@gmail.com>
2019-12-07 20:56:34 -0600

    flake8 fixed :(

--- tests/netting/test_client_init.py
+++ tests/netting/test_client_init.py
@@ -5,10 +5,14 @@ import apps.netting.src.client as clients
 """
 Test that everything is initilzed correctly for the application.
 """
-def test_init():
-	data_gen.main()
-	clis = clients.init_clients()
 
-	for cli in clis:
-		assert cli.balance >= 0, "Init balance >= 0"
-		assert len(cli.out_tx) + len(cli.in_tx) >= 1, "With high probability must have atleast one tx"
+
+def test_init():
+    data_gen.main()
+    clis = clients.init_clients()
+
+    for cli in clis:
+        assert cli.balance >= 0, "Init balance >= 0"
+        assert (
+            len(cli.out_tx) + len(cli.in_tx) >= 1
+        ), "With high probability must have atleast one tx"
