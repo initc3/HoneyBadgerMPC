347b8af41117115ff599b97e9b52a81b5a936396
Sylvain Bellemare <sbellem@gmail.com>
2020-04-04 22:07:48 -0500

    wip: setup phase writes common config file
    The setup phase for now is responsible to deploy the contract and to
    provide the contract address to the MPC servers and clients.
    
    Additionaly, the contract deployer, the MPC servers and clients, need
    an eth address and for now a "dummy" eth address is assigned to each
    participant. The addresses can be added to the common config file.
    
    next:
    * make sure all common data is in the config file
    * update client config with just client config data
    * use config for mpc network

--- apps/masks/setup_phase.py
+++ apps/masks/setup_phase.py
@@ -1,5 +1,9 @@
+import argparse
 import logging
+import pprint
 from pathlib import Path
+
+import toml
 
 from web3 import HTTPProvider, Web3
 
@@ -12,16 +16,26 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
+def set_eth_addrs(config_dict, config_filepath):
+    """Set eth addresses for the contract deployer, the MPC servers and
+    the client and update the given config file.
+
+    Parameters
+    ----------
+    config_dict : dict
+        Configuration dict to update with eth addresses.
+    config_filepath : str
+        Toml file path to the configuration to update.
+    """
+    raise NotImplementedError
+
+
 def deploy_contract(
-    *, contract_name, contract_filepath, n=4, t=1, eth_rpc_hostname, eth_rpc_port
+    w3, *, contract_name, contract_filepath, n=4, t=1, deployer_addr, mpc_addrs
 ):
-    w3_endpoint_uri = f"http://{eth_rpc_hostname}:{eth_rpc_port}"
-    w3 = Web3(HTTPProvider(w3_endpoint_uri))
-    deployer = w3.eth.accounts[49]
-    mpc_addrs = w3.eth.accounts[:n]
     contract_address, abi = create_and_deploy_contract(
         w3,
-        deployer=deployer,
+        deployer=deployer_addr,
         contract_name=contract_name,
         contract_filepath=contract_filepath,
         args=(mpc_addrs, t),
@@ -35,25 +49,58 @@ if __name__ == "__main__":
     # level as `.warning()` works.
     logger.info(f"Deploying contract ...")
     print(f"Deploying contract ...")
-    contract_name = "MpcCoordinator"
-    contract_filename = "contract.sol"
+
+    default_config_path = PARENT_DIR.joinpath("public-data/config.toml")
+    parser = argparse.ArgumentParser(description="Setup phase.")
+    parser.add_argument(
+        "-c",
+        "--config-file",
+        default=str(default_config_path),
+        help=f"Configuration file to use. Defaults to '{default_config_path}'.",
+    )
+    args = parser.parse_args()
+    config_file = args.config_file
+    config = toml.load(config_file)
+    print(config)
+
+    n = config["n"]
+    t = config["t"]
+    eth_config = config["eth"]
+    contract_name = eth_config["contract_name"]
+    contract_filename = eth_config["contract_filename"]
     contract_filepath = PARENT_DIR.joinpath(contract_filename)
-    eth_rpc_hostname = "blockchain"
-    eth_rpc_port = 8545
-    n, t = 4, 1
+    eth_rpc_hostname = eth_config["rpc_host"]
+    eth_rpc_port = eth_config["rpc_port"]
+    w3_endpoint_uri = f"http://{eth_rpc_hostname}:{eth_rpc_port}"
+    w3 = Web3(HTTPProvider(w3_endpoint_uri))
+
+    deployer_addr = w3.eth.accounts[49]
+    mpc_addrs = []
+    for s in config["servers"]:
+        mpc_addr = w3.eth.accounts[s["id"]]
+        s["eth_address"] = mpc_addr
+        mpc_addrs.append(mpc_addr)
+
     contract_address = deploy_contract(
+        w3,
         contract_name=contract_name,
         contract_filepath=contract_filepath,
-        t=1,
-        eth_rpc_hostname=eth_rpc_hostname,
-        eth_rpc_port=eth_rpc_port,
+        n=n,
+        t=t,
+        deployer_addr=deployer_addr,
+        mpc_addrs=mpc_addrs,
     )
+    config["deployer_address"] = deployer_addr
+    config["eth"]["contract_address"] = contract_address
     logger.info(f"Contract deployed at address: {contract_address}")
     print(f"Contract deployed at address: {contract_address}")
+
+    with open(config_file, "w") as f:
+        toml.dump(config, f)
     with open(CONTRACT_ADDRESS_FILEPATH, "w") as f:
         f.write(contract_address)
+
     logger.info(f"Wrote contract address to file: {CONTRACT_ADDRESS_FILEPATH}")
     print(f"Wrote contract address to file: {CONTRACT_ADDRESS_FILEPATH}")
-    import time
-
-    time.sleep(10)
+    print(f"\nUpdated common config file: {config_file}\n")
+    pprint.pprint(config)
