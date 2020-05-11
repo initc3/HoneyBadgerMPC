d663e746878cda91c443cd1c15b5843b631f343b
Sylvain Bellemare <sbellem@gmail.com>
2020-05-10 19:05:17 -0500

    Set absolute path for benchmark logs
    Fixes #440.
    
    The problem is that when docs are generated with autodoc the reference
    point to determine the relative location of the benchmark-logs/ dir is
    different.

--- honeybadgermpc/__init__.py
+++ honeybadgermpc/__init__.py
@@ -1,7 +1,6 @@
 """HoneyBadgerMPC: Confidentiality Layer for Consortium Blockchains."""
 
 import logging.config
-import os
 from pathlib import Path
 
 import yaml
@@ -10,8 +9,9 @@ import yaml
 CURRENT_DIR = Path(__file__).resolve().parent
 ROOT_DIR = CURRENT_DIR.parent
 
+print(f"ROOT dir: {ROOT_DIR}")
+
 with open(CURRENT_DIR / "logging.yaml", "r") as f:
-    os.makedirs(ROOT_DIR / "benchmark-logs", exist_ok=True)
     logging_config = yaml.safe_load(f.read())
     logging.config.dictConfig(logging_config)
     logging.getLogger("asyncio").setLevel(logging.WARNING)
